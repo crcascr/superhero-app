c7dd3ba359d7b25af6ee829c55ae350612b1b4a0
crcascr <cristianj_20@hotmail.com>
2025-05-28 18:28:01 -0500

    refactor(api): refactor hooks to be able of refetch data

--- hooks/use-get-superhero.ts
+++ hooks/use-get-superhero.ts
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { getSuperhero } from "@/services";
 import { GetSuperheroProps, Superhero, ErrorResponse } from "@/interfaces";
 
@@ -8,23 +8,23 @@ export const useGetSuperhero = ({ id }: GetSuperheroProps) => {
   const [error, setError] = useState<ErrorResponse | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
-  useEffect(() => {
-    const fetchSuperhero = async () => {
-      setLoading(true);
-      setError(null);
-      const response = await getSuperhero({ id });
+  const fetch = useCallback(async () => {
+    setLoading(true);
+    setError(null);
+    const response = await getSuperhero({ id });
 
-      if (response.success) {
-        setData(response.success);
-      } else {
-        setError(response.error ?? { status: 500, message: "Unknown error" });
-      }
+    if (response.success) {
+      setData(response.success);
+    } else {
+      setError(response.error ?? { status: 500, message: "Unknown error" });
+    }
 
-      setLoading(false);
-    };
-
-    fetchSuperhero();
+    setLoading(false);
   }, [id]);
 
-  return { data, error, loading };
+  useEffect(() => {
+    fetch();
+  }, [fetch]);
+
+  return { data, error, loading, refetch: fetch };
 };
--- hooks/use-get-superheros.ts
+++ hooks/use-get-superheros.ts
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { getSuperheros } from "@/services";
 import {
   GetSuperherosProps,
@@ -12,23 +12,23 @@ export const useGetSuperheroes = ({ page, size }: GetSuperherosProps) => {
   const [error, setError] = useState<ErrorResponse | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
-  useEffect(() => {
-    const fetchSuperheroes = async () => {
-      setLoading(true);
-      setError(null);
-      const response = await getSuperheros({ page, size });
+  const fetch = useCallback(async () => {
+    setLoading(true);
+    setError(null);
+    const response = await getSuperheros({ page, size });
 
-      if (response.success) {
-        setData(response.success);
-      } else {
-        setError(response.error ?? { status: 500, message: "Unknown error" });
-      }
+    if (response.success) {
+      setData(response.success);
+    } else {
+      setError(response.error ?? { status: 500, message: "Unknown error" });
+    }
 
-      setLoading(false);
-    };
-
-    fetchSuperheroes();
+    setLoading(false);
   }, [page, size]);
 
-  return { data, error, loading };
+  useEffect(() => {
+    fetch();
+  }, [fetch]);
+
+  return { data, error, loading, refetch: fetch };
 };
