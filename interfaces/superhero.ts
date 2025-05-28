b43708dc7589dafece38480434cfacce21a7f469
crcascr <cristianj_20@hotmail.com>
2025-05-28 17:04:07 -0500

    feat(api): add superhero and superhero response interfaces

---
+++ interfaces/superhero.ts
@@ -0,0 +1,55 @@
+export interface Superhero {
+  id: number;
+  name: string;
+  slug: string;
+  powerstats: {
+    intelligence: number;
+    strength: number;
+    speed: number;
+    durability: number;
+    power: number;
+    combat: number;
+  };
+  appearance: {
+    gender: string;
+    race: string | null;
+    height: string[];
+    weight: string[];
+    eyeColor: string;
+    hairColor: string;
+  };
+  biography: {
+    fullName: string;
+    alterEgos: string;
+    aliases: string[];
+    placeOfBirth: string;
+    firstAppearance: string;
+    publisher: string;
+    alignment: string;
+  };
+  work: {
+    occupation: string;
+    base: string;
+  };
+  connections: {
+    groupAffiliation: string;
+    relatives: string;
+  };
+  images: {
+    xs: string;
+    sm: string;
+    md: string;
+    lg: string;
+  };
+}
+
+export interface SuperheroListResponse {
+  length: number;
+  size: number;
+  page: number;
+  firstPage: number;
+  lastPage: number;
+  startIndex: number;
+  endIndex: number;
+  items: Superhero[];
+}
